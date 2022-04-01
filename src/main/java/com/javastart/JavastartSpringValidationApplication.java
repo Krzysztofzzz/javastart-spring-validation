f3a9afd9957e8fdfb4e010b12d1e755cc2091d71
Krzysztof <krzubrycki@gmail.com>
2022-04-01 21:06:00 +0200

    created project

---
+++ src/main/java/com/javastart/JavastartSpringValidationApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartSpringValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartSpringValidationApplication.class, args);
+    }
+
+}
