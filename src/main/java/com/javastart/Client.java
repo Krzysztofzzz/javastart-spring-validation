9f1bad3a62ed1e60e84d197ee10deed3a772e14d
Krzysztof <krzubrycki@gmail.com>
2022-04-03 13:52:33 +0200

    added bean validation. ConstraintViolationException

--- src/main/java/com/javastart/Client.java
+++ src/main/java/com/javastart/Client.java
@@ -4,16 +4,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 
 @Entity
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min = 2,max = 100)
     private String firstName;
+    @NotNull
+    @Size(min = 2,max = 100)
     private String lastName;
+    @NotNull
+    @Email
+    @Size(min = 5,max = 100)
     private String email;
+    @Min(value = 1)
     private int age;
+    @NotNull
+    @Pattern(regexp = "[A-Z]{4}[0-9]{3}")
     private String username;
 
     public Client() {
